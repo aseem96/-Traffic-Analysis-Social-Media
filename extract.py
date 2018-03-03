3c9111aedf075463ab2b6920389f15205f3a4ee8
Aseem Raina <aseemraina1996@gmail.com>
2018-03-03 18:49:00 -0500

    added remaining files

---
+++ extract.py
@@ -0,0 +1,43 @@
+import os
+#from nltk.tag import StanfordNERTagger
+from nltk.tag.stanford import NERTagger
+from nltk.corpus import stopwords
+from incidents import arr
+import string
+
+#st = StanfordNERTagger('english.conll.4class.distsim.crf.ser.gz')
+st = NERTagger('/home/nehal/Downloads/nertagger/classifiers/english.conll.4class.distsim.crf.ser.gz','/home/nehal/Downloads/nertagger/stanford-ner.jar')
+
+def get_location(text):
+	loc = ""
+	text = text.translate(None, string.punctuation)
+	tags = st.tag(text.split())
+	for tag in tags:
+		if(tag[1] == u'LOCATION' or tag[1] == u'ORGANIZATION' or tag[1] == u'PERSON'):
+			loc += tag[0]
+			loc += " "
+	return loc
+
+def filter_stopwords(text):
+	stop = set(stopwords.words('english'))
+	return [i for i in text.split() if i not in stop]
+
+def get_reason(text):
+	rsn = ""
+	text = text.translate(None, string.punctuation)
+	for word in text.split():
+		if word in arr:
+			rsn += str(word)
+			rsn += " "
+	return rsn
+
+def main():
+	for line in open("/home/nehal/Desktop/traffic_samr/tweets.txt"):
+		print "TEXT: "+str(line).replace("\n","")
+		print "AFTER REMOVING STOPWORDS: "+str(filter_stopwords(line))
+		print "LOCATION: "+str(get_location(line))
+		print "REASON: "+str(get_reason(line))
+		print "\n"
+
+if __name__ == '__main__':
+	main()
